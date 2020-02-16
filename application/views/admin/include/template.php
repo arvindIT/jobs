a85cb9a678c598122cbff6c86da6e3a5daf483d2
Arvind <arvindprajapati6087@gmail.com>
2020-02-16 08:53:55 +0530

    testing

--- application/views/admin/include/template.php
+++
@@ -1,7 +0,0 @@
-<?php
-$this->load->view('admin/include/header');
-//$this->load->view('admin/include/left');
-
-$this->load->view($main_content);
-$this->load->view('admin/include/footer');
-?>
--- application/views/front/include/template.php
+++
@@ -1,4 +0,0 @@
-<?php
-$this->load->view('front/include/header');
-$this->load->view($main_content);
-$this->load->view('front/include/footer');
