517e66cb1d24cf0af03c4335365643f3fdba835b
arvind <arvind@topsinfosolutions.com>
2020-02-18 10:29:33 +0530

    db changes

--- application/config/database.php
+++ application/config/database.php
@@ -54,10 +54,10 @@ if(($_SERVER['HTTP_HOST']==('naukari-hub.com')) || $_SERVER['HTTP_HOST']==('www.
 	$db['default']['password'] = '@sbGQI6)5rKW';
 	$db['default']['database'] = 'naukari_hub';
 }elseif($_SERVER['HTTP_HOST']==('localhost' || '192.168.0.13' || '192.168.0.33' || '192.168.0.41' || '192.168.0.28' || '192.168.0.143' || '192.168.0.14')){
-	$db['default']['hostname'] = 'localhost';
+	$db['default']['hostname'] = '18.223.155.214';
 	$db['default']['username'] = 'root';
 	$db['default']['password'] = '';
-	$db['default']['database'] = 'naukari_hub';
+	$db['default']['database'] = 'simpleway';
 }
 
 
